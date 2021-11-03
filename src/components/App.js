1d11e3ddf92e5f95221f9baada5ce303a4596ed9
KCamades <you@example.com>
2021-11-03 17:51:22 +0900

    create config 2

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import config from '../config.json';
 export default () => {
   const [name, setName] = useState('');
 
-  if(config.singnInEnabled && name === '') {
+  if(config.signInEnabled && name === '') {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
